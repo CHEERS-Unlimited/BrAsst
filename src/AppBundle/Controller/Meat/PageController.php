7c94b44bc6b92e8b83d1a9e2267dffd7400ea496
Grim Volk <grimv01k@gmail.com>
2014-12-25 15:04:12 +0200

    Version
    gets version

--- src/AppBundle/Controller/Meat/PageController.php
+++ src/AppBundle/Controller/Meat/PageController.php
@@ -10,6 +10,8 @@ use DeviceDetector\DeviceDetector;
 use DeviceDetector\Parser\Device\DeviceParserAbstract;
 use DeviceDetector\Cache\CacheFile;
 
+use Goutte\Client;
+
 class PageController extends Controller
 {
     /**
@@ -45,6 +47,35 @@ class PageController extends Controller
                 $clientInfo, $osInfo, $device, $brand, $model
             );
         }
+
+        $client = new Client();
+        $crawler = $client->request('GET', "https://en.wikipedia.org/wiki/Google_Chrome");
+        //$crawler = $client->request('GET', "https://en.wikipedia.org/wiki/Firefox");
+        //$crawler = $client->request('GET', "https://en.wikipedia.org/wiki/Opera_(web_browser)");
+        //$crawler = $client->request('GET', "https://en.wikipedia.org/wiki/Internet_Explorer");
+        //$crawler = $client->request('GET', "https://en.wikipedia.org/wiki/Safari_(web_browser)");
+        $status_code = $client->getResponse()->getStatus();
+        if($status_code==200){
+            //process the documents
+            $tableBlock = $crawler->filter('body > div#content > div#bodyContent > div#mw-content-text > table.infobox > tr');
+
+            //var_dump( $crawler->filter('body > div#content > div#bodyContent > div#mw-content-text > table.infobox > tr')->eq(3)->text() );
+            //var_dump( $crawler->filter('body > div#content > div#bodyContent > div#mw-content-text > table.infobox > tr')->eq(4)->text() );
+
+            $siblingsNumber = count($tableBlock->siblings());
+
+            for($i = 1; $i < $siblingsNumber; $i++)
+            {
+                if( count($tableBlock->eq($i)->filter('th')) && $tableBlock->eq($i)->filter('th')->text() == 'Stable release' ) {
+                    $stableRelease = $tableBlock->eq($i)->filter('td')->text();
+                    break;
+                }
+            }
+        }
+
+        preg_match_all("/([0-9]+\.)([0-9]\.?)+/", $stableRelease, $matches, PREG_PATTERN_ORDER);
+
+        var_dump($stableRelease, $matches[0][0]);
 
         return $this->render('AppBundle:Meat:index.html.twig');
     }
