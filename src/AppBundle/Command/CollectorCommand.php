98448a7fd5fef2b021be1a03e7adba534f1d8b8d
Grim Volk <grimv01k@gmail.com>
2014-12-27 15:43:31 +0200

    Build 4
    2014/12/27 15:43

---
+++ src/AppBundle/Command/CollectorCommand.php
@@ -0,0 +1,75 @@
+<?php
+# src/AppBundle/Command/CollectorCommand.php
+namespace AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand,
+    Symfony\Component\Console\Input\InputArgument,
+    Symfony\Component\Console\Input\InputInterface,
+    Symfony\Component\Console\Input\InputOption,
+    Symfony\Component\Console\Output\OutputInterface;
+
+class CollectorCommand extends ContainerAwareCommand
+{
+    const TASK_UPDATE_STABLE_RELEASE = 'stable_release';
+    const TASK_UPDATE_MARKET_SHARE   = 'market_share';
+
+    protected function configure()
+    {
+        $this
+            ->setName('brasst:collect')
+            ->setDescription('Update system with important collected web data')
+            ->addArgument('task', InputArgument::REQUIRED, 'What update task to execute?')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $task = $input->getArgument('task');
+
+        switch($task)
+        {
+            case self::TASK_UPDATE_STABLE_RELEASE:
+                $this->updateStableRelease();
+            break;
+
+            case self::TASK_UPDATE_MARKET_SHARE:
+                $this->updateMarketShare();
+            break;
+        }
+
+        $output->writeln('Done!');
+    }
+
+    private function updateStableRelease()
+    {
+        $_collector = $this->getContainer()->get('collector');
+
+        try {
+            $_collector->updateBrowserStableRelease();
+
+            $this->logResult("***CollectorCommand***: Stable Release updated");
+        } catch(\Exception $e) {
+            $this->logResult("***CollectorCommand***: Failed to update Stable Release with exception - " . $e->getMessage());
+        }
+    }
+
+    private function updateMarketShare()
+    {
+        $_collector = $this->getContainer()->get('collector');
+
+        try {
+            $_collector->updateBrowsersMarketShare();
+
+            $this->logResult("***CollectorCommand***: Market Share updated");
+        } catch(\Exception $e) {
+            $this->logResult("***CollectorCommand***: Failed to update Market Share with exception - " . $e->getMessage());
+        }
+    }
+
+    private function logResult($result)
+    {
+        $_logger = $this->getContainer()->get('logger');
+
+        $_logger->info($result);
+    }
+}
